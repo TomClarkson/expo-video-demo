704a4934099352dee4b61d0ee53557f71fe9f3c6
Tom Clarkson <tom@designsight.co.nz>
2022-06-15 14:23:26 +1200

    get it working on android

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { ReanimatedPlayer } from "./ReanimatedPlayer";
 
 export default function App() {
   return (
-    <SafeAreaProvider style={{ flex: 1 }}>
-      <StatusBar hidden />
-      <View style={styles.container}>
-        <ReanimatedPlayer />
-      </View>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider style={{ flex: 1 }}>
+        <StatusBar hidden />
+        <View style={styles.container}>
+          <ReanimatedPlayer />
+        </View>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
 
